22b2dbeede3ebd56931abbf7bfa6579288330891
danielebd <danielebalebalestro@gmail.com>
2023-02-21 17:35:41 +0100

    to fixed non funziona

--- js/main.js
+++ js/main.js
@@ -2,29 +2,32 @@
 
 //inserisci kilometraggio da percorrere
 const percorso = parseInt(prompt('Iserisci km da persorrere'))
-console.log(percorso)
+console.log('km da percorrere: ' + percorso)
 
 //inserisci età passeggero
 const eta = parseInt(prompt('Iserisci età'))
-console.log(eta)
+console.log('età passeggero: ' + eta)
 
 //calcolo prezzo senza applicazione sconto
 let prezzo = (percorso * 0.21)
-console.log(prezzo)
+console.log('prezzo senza sconti applicati: ' + prezzo + '€')
 
 /* calcolo prezzo sulle fasce di età così da applicare la scontistica 
 nel caso in cui il passeggero rientri nelle categorie agevolate  */
 if(eta >= 65) {
-    console.log(prezzo * (60 / 100))
+    console.log('prezzo scontato over 65 è di:' + prezzo * (60 / 100) + '€')
 }
 
 else if(eta < 18) {
-    console.log(prezzo * (80 / 100))
+    console.log('prezzo scontato per i minori 18 è di: ' + prezzo * (80 / 100) + '€')
 }
 
 else {
-    console.log(prezzo)
+    
 }
+
+
+prezzo.toFixed(2);
 
 //stampare prezzo finale della tratta su console e pagina
 console.log ('il prezzo del biglietto è ' + prezzo + '€')
